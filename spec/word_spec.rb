77969e7db00c21489166d07c60a153fd89885fcd
Tola's Terminal <sohbaker@gmail.com>
2018-11-06 14:12:19 +0000

    spec can take a guess letter and map it against the answer

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -25,4 +25,33 @@ RSpec.describe Word do
     expect(word.guessed_letters).to eq(['h', '_', '_', '_', '_'])
   end
 
+  it 'adds another letter to the current word' do
+    word = Word.new('hello')
+
+    word.add_guess('h')
+    word.add_guess('e')
+
+    expect(word.guessed_letters).to eq(['h', 'e', '_', '_', '_'])
+  end
+
+  it 'adds another letter to the current word in the right position' do
+    word = Word.new('hello')
+
+    word.add_guess('h')
+    word.add_guess('e')
+    word.add_guess('o')
+
+    expect(word.guessed_letters).to eq(['h', 'e', '_', '_', 'o'])
+  end
+
+  it 'adds another letter to the current word in the right position' do
+    word = Word.new('hello')
+
+    word.add_guess('h')
+    word.add_guess('e')
+    word.add_guess('o')
+    word.add_guess('l')
+
+    expect(word.guessed_letters).to eq(['h', 'e', 'l', 'l', 'o'])
+  end
 end
