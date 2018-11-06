9c197e1fe8f9b96f7ea8417d1585827513c9993e
Tola's Terminal <sohbaker@gmail.com>
2018-11-06 17:14:36 +0000

    tried to count guesses but need to rethink logic

--- lib/word.rb
+++ lib/word.rb
@@ -1,32 +1,60 @@
 class Word
   def initialize(word)
     @word = word
+    # @word_split = word.split('')
     @user_guesses = []
+    @lives = 5
+    # @guesses_count = 0
+
   end
 
   def add_guess(letter)
     @user_guesses.push(letter)
+    # @guesses_count = +1
+
+    # if @word_split.include?(letter) == false
+    #   p @word_split
+    #   @lost_lives << 'x'
+    #   p @lost_lives
+    # end
   end
 
   def guessed_letters
     # ['_', '_', '_', '_', '_']
-    letters = []
-    @word.split('').each do |letter|
-      if @user_guesses.include?(letter)
-        letters.push(letter)
-      else
-        letters.push('_')
+    # wrong_letters = []
+    reveal_correct_letters = []
+
+    if @guesses_count < 5
+      @word.split('').each do |letter|
+        if @user_guesses.include?(letter)
+          reveal_correct_letters.push(letter)
+          @lives -= 1
+          # reveal_correct_letters -= @user_guesses - can't use this because it reduces the number of arguments expected
+        else
+          reveal_correct_letters.push('_')
+          # @lost_lives << 'x'
+          # wrong_letters.push()
+          # print "#{@user_guesses} Wrong letter. Guess again"
+          # p wrong_letters
+        end
+      end
+
+      if reveal_correct_letters == @word.split('')
+        puts "You have WON!"
+        puts "The answer is #{@word}!"
+      end
+
+      if @guesses_count == 5
+        puts "YOU LOSE!"
+        puts "The answer was #{@word}!"
       end
     end
 
-    if letters == @word.split('')
-      puts "You have WON!"
-      puts "The answer is #{@word}!"
-    end
-    letters
-  end
-    # @word.split('').map do |letter|
-    #   '_'
-  # end
+    p reveal_correct_letters
+    reveal_correct_letters
 
+  end
+  #   @word.split('').map do |letter|
+  #     '_'
+  # end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,77 +1,79 @@
 require 'word'
 
 RSpec.describe Word do
-  it 'returns the guessed letters' do
-    word = Word.new('hello')
-
-    guessed_letters = word.guessed_letters
-
-    expect(guessed_letters).to eq(['_', '_', '_', '_', '_'])
-  end
-
-  it 'returns empty letters for different lengths of word' do
-    word = Word.new('blackberry')
-
-    guessed_letters = word.guessed_letters
-
-    expect(guessed_letters).to eq(['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'])
-  end
-
-  it 'adds a letter to the current word' do
-    word = Word.new('hello')
-
-    word.add_guess('h')
-
-    expect(word.guessed_letters).to eq(['h', '_', '_', '_', '_'])
-  end
-
-  it 'adds another letter to the current word' do
-    word = Word.new('hello')
-
-    word.add_guess('h')
-    word.add_guess('e')
-
-    expect(word.guessed_letters).to eq(['h', 'e', '_', '_', '_'])
-  end
-
-  it 'adds another letter to the current word in the right position' do
-    word = Word.new('hello')
-
-    word.add_guess('h')
-    word.add_guess('e')
-    word.add_guess('o')
-
-    expect(word.guessed_letters).to eq(['h', 'e', '_', '_', 'o'])
-  end
-
-  it 'adds another letter to the current word in the right position' do
-    word = Word.new('hello')
-
-    word.add_guess('h')
-    word.add_guess('e')
-    word.add_guess('o')
-    word.add_guess('l')
-
-    expect(word.guessed_letters).to eq(['h', 'e', 'l', 'l', 'o'])
-  end
-
-  it 'tells the user they have won when they\'ve guessed the full word' do
-    word = Word.new("hello")
-
-    word.add_guess('h')
-    word.add_guess('e')
-    word.add_guess('o')
-    word.add_guess('l')
-
-    expect(word.guessed_letters).to eq(['h', 'e', 'l', 'l', 'o'])
-  end
-
-  # it 'returns nothing if the guess is incorrect' do
+#----right answer
+  # it 'returns the guessed letters' do
   #   word = Word.new('hello')
   #
-  #   word.add_guess('k')
+  #   guessed_letters = word.guessed_letters
   #
-  #   expect(word.guessed_letters).to eq(['_', '_', '_', '_', '_'])
+  #   expect(guessed_letters).to eq(['_', '_', '_', '_', '_'])
   # end
   #
+  # it 'returns empty letters for different lengths of word' do
+  #   word = Word.new('blackberry')
+  #
+  #   guessed_letters = word.guessed_letters
+  #
+  #   expect(guessed_letters).to eq(['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'])
+  # end
+  #
+  # it 'adds a letter to the current word' do
+  #   word = Word.new('hello')
+  #
+  #   word.add_guess('h')
+  #
+  #   expect(word.guessed_letters).to eq(['h', '_', '_', '_', '_'])
+  # end
+  #
+  # it 'adds a letter to the current word in the right position' do
+  #   word = Word.new('hello')
+  #
+  #   word.add_guess('h')
+  #   word.add_guess('e')
+  #   word.add_guess('o')
+  #
+  #   expect(word.guessed_letters).to eq(['h', 'e', '_', '_', 'o'])
+  # end
+  #
+  # it 'tells the user they have won when they\'ve guessed the full word' do
+  #   word = Word.new("hello")
+  #
+  #   word.add_guess('h')
+  #   word.add_guess('e')
+  #   word.add_guess('o')
+  #   word.add_guess('l')
+  #
+  #   expect(word.guessed_letters).to eq(['h', 'e', 'l', 'l', 'o'])
+  # end
+# ----wrong answer
+  it 'returns empty array if the guess is incorrect' do
+    word = Word.new('snack')
+
+    word.add_guess('g')
+
+    expect(word.guessed_letters).to eq(['_', '_', '_', '_', '_'])
+  end
+
+  it 'can handle incorrect guess followed by correct guess' do
+    word = Word.new('snack')
+
+    word.add_guess('g')
+    word.add_guess('a')
+
+    expect(word.guessed_letters).to eq(['_', '_', 'a', '_', '_'])
+  end
+
+  it 'tells the user when they\'ve run out of guesses' do
+    word = Word.new('snack')
+
+    word.add_guess('g')
+    word.add_guess('l')
+    word.add_guess('b')
+    word.add_guess('d')
+    word.add_guess('e')
+
+    expect(word.guessed_letters).to eq(['_', '_', '_', '_', '_'])
+  end
+
 end
